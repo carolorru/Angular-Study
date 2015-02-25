8612ad981224ff7ed529b27b8254ae6ed9061178
Carol <carolorru@hotmail.com>
2015-02-25 08:54:35 -0300

    view de separação

--- portal-niase/app/controllers/controllers.js
+++ portal-niase/app/controllers/controllers.js
@@ -1,105 +1,3 @@
-/*appLogin
-	.constant('AUTH_EVENTS', {
-		loginSuccess: 'auth-login-success',
-		loginFailed: 'auth-login-failed',
-		logoutSuccess: 'auth-logout-success',
-		sessionTimeout: 'auth-session-timeout',
-		notAuthenticated: 'auth-not-authenticated',
-		notAuthorized: 'auth-not-authorized'
-	})
-	.constant('USER_ROLES', {
-		admin: 'admin',
-		user: 'user',
-		guest: 'guest'
-	});
-
-appLogin
-.factory('AuthService', function ($http, Session) {
-    var authService = {};
-    authService.login = function (credentials) {
-        return $http.post('/login' + credentials)
-            .then(function (res) {
-                Session.create(res.data);
-            });
-    };
-
-    authService.isAuthenticated = function () {
-        return !!Session.user;
-    };
-
-    authService.isAuthorized = function (authorizedRoles) {
-        if (!angular.isArray(authorizedRoles)) {
-            authorizedRoles = [authorizedRoles];
-        }
-        return (authService.isAuthenticated() &&
-            authorizedRoles.indexOf(Session.userRole));
-    };
-
-    return authService;
-}).factory('AuthInterceptor', function ($q, Session) {
-    return {
-        request: function (config) {
-            config.headers['Authorization'] = 'Bearer ' + Session.access_token;
-            return config || $q.when(config);
-        }
-    };
-});
-
-
-appLogin.service('Session', function ($rootScope, $cookieStore, AUTH_EVENTS) {
-    var storedSession = $cookieStore.get('userSession');
-    var that = this;
-
-    if(storedSession) {
-        this.access_token = storedSession.access_token;
-        this.user = storedSession.user;
-    }
-
-    this.create = function (sessionData) {
-        this.access_token = sessionData.access_token;
-        this.user = sessionData.user;
-        $cookieStore.put('userSession', sessionData);
-    };
-
-    this.destroy = function () {
-        that.access_token = null;
-        that.user = null;
-        $cookieStore.remove('userSession');
-    };
-
-    $rootScope.$on(AUTH_EVENTS.notAuthenticated, this.destroy);
-    $rootScope.$on(AUTH_EVENTS.sessionTimeout, this.destroy);
-
-    return this;
-});
-
-appLogin.config(function ($httpProvider) {
-    $httpProvider.interceptors.push([
-        '$injector',
-        function ($injector) {
-            return $injector.get('AuthInterceptor');
-        }
-    ]);
-});
-
-
-appLogin.controller('LoginCtrl', function ($scope, $rootScope, AUTH_EVENTS, AuthService, $location) {
-	console.log($rootScope);
-    $scope.credentials = {
-        username: '',
-        password: ''
-    };
-    $scope.login = function (credentials) {
-        AuthService.login(credentials).then(function () {
-            $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
-            $location.path('/portal-niase/');
-        }, function () {
-            $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
-        });
-    };
-});*/
-
-
 app.controller('HomeCtrl', function($rootScope, $location)
 {
    $rootScope.activetab = $location.path();
@@ -110,13 +8,26 @@ app.controller('LoginCtrl', function($rootScope, $location)
    $rootScope.activetab = $location.path();
 });
 
+
 app.controller('SeparacaoCtrl', function($rootScope, $location, $scope, $window, $http)
 {
-    $.get('http://webtalk.com.br/uperp/pedidos/separados').success(function(data){
-            $scope.lista = data;
-        });
-   $rootScope.activetab = $location.path();
+    var json;
+    $http.get('http://webtalk.com.br/uperp/pedidos/a-separar').success(function(data){
+        json = data;
+        $scope.aSeparar = json;
+    });
+    $http.get('http://webtalk.com.br/uperp/pedidos/em-separacao').success(function(data){
+        json = data;
+        $scope.emSeparacao = json;
+    });
+    $http.get('http://webtalk.com.br/uperp/pedidos/separados').success(function(data){
+        json = data;
+        $scope.separados = json;
+    });
+    $rootScope.activetab = $location.path();
 });
+
+
 
 app.controller('ConferenciaCtrl', function($rootScope, $location)
 {
