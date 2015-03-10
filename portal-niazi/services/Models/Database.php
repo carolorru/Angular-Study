96cefb7b0b3d5cc333c7650c9e92f3928bda683c
marcelobe <marcelo.elias@inkuba.com.br>
2015-03-09 21:51:29 -0300

    implementacao do sqlsrv

--- portal-niazi/services/Models/Database.php
+++ portal-niazi/services/Models/Database.php
@@ -53,8 +53,24 @@ class Database
 			$this->tbl->conferencia = 'PAINEL_ACD_002';
 			$this->tbl->expedicao   = 'PAINEL_ACD_003';
 
-			$this->mssql = @mssql_connect($this->dbhost,$this->user,$this->password) or die("Não foi possível a conexão com o banco de dados!");
-			@mssql_select_db($this->db,$this->mssql) or die("Não foi possível selecionar o banco de dados!");
+			//$this->mssql = @mssql_connect($this->dbhost,$this->user,$this->password) or die("Não foi possível a conexão com o banco de dados!");
+			//@mssql_select_db($this->db,$this->mssql) or die("Não foi possível selecionar o banco de dados!");
+
+			$this->dbtype   = "sqlsrv";
+			$connectionInfo = array("UID" => $this->user, "PWD" => $this->password, "Database" => $this->db);
+			$this->sqlsrv   = sqlsrv_connect($this->dbhost, $connectionInfo); 
+			
+			if($conn === false)
+			{
+
+				echo "Unable to connect.";
+				die( print_r( sqlsrv_errors(), true)); 
+			 
+			}else{
+
+				echo "aqui está normal";
+
+			}
 
 		}
 
@@ -67,8 +83,15 @@ class Database
 
 		if($this->dbtype == 'mysql')
 		{
+
 			$error = mysql_error();
 			return $error;
+
+		}else if($this->dbtype == 'sqlsrv'){
+
+			$error = sqlsrv_errors();
+			return $error;
+
 		}
 
 		$error = mssql_get_last_message();
@@ -81,8 +104,15 @@ class Database
 
 		if($this->dbtype == 'mysql')
 		{
+
 			$row = mysql_fetch_array($query);
 			return $row;
+
+		}else if($this->dbtype == 'sqlsrv'){
+
+			$error = sqlsrv_fetch_array($query);
+			return $error;
+
 		}
 
 		$row = mssql_fetch_array($query);
@@ -95,8 +125,15 @@ class Database
 
 		if($this->dbtype == 'mysql')
 		{
+
 			$num = mysql_num_rows($query);
 			return $num;
+
+		}else if($this->dbtype == 'sqlsrv'){
+
+			$num = sqlsrv_num_rows($query);
+			return $num;
+
 		}
 
 		$num = mssql_num_rows($query);
@@ -109,8 +146,15 @@ class Database
 		
 		if($this->dbtype == 'mysql')
 		{
+
 			$query = mysql_query($params,$this->mssql);
 			return $query;
+
+		}else if($this->dbtype == 'sqlsrv'){
+
+			$num = sqlsrv_query($params,$this->sqlsrv);
+			return $num;
+			
 		}
 
 		$query = mssql_query($params);
