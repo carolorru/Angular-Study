30ec70379860492dad1a7fef91b484827ee60f61
Caroline Orrù <contato@carolineorru.com.br>
2015-04-09 17:20:04 -0300

    Adicionado DT de Conferencia

--- portal-niazi/services/Models/Conferencia.php
+++ portal-niazi/services/Models/Conferencia.php
@@ -37,10 +37,11 @@ class Conferencia
 		max(isnull(CB9_CODSEP, ''))	as COD_CONFERENTE, 
 		max(isnull(CB1_NOME,''))	as NOM_CONFERENTE, 
 		1000                        as META_CONF,
-		sum(C5_PBRUTO)				as PESO_BRUTO 
+		sum(C5_PBRUTO)				as PESO_BRUTO,
+		CB7_DTFIMS 					as DT_SEPARACAO 
 from (
 
-SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C9_QTDLIB, CB7_PEDIDO, CB7_XHRIC, CB7_XDTIC, CB7_XHRFC, CB7_XDTFC, sum(isnull(CB8_QTDORI-CB8_SALDOE,0)) as CB9_QTESEP, max(isnull(CB7_XOPERC, '')) as CB9_CODSEP, max(isnull(CB1_NOME,'')) as CB1_NOME, 
+SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C9_QTDLIB, CB7_PEDIDO, CB7_DTFIMS, CB7_XHRIC, CB7_XDTIC, CB7_XHRFC, CB7_XDTFC, sum(isnull(CB8_QTDORI-CB8_SALDOE,0)) as CB9_QTESEP, max(isnull(CB7_XOPERC, '')) as CB9_CODSEP, max(isnull(CB1_NOME,'')) as CB1_NOME, 
 (select SC5a.C5_PBRUTO from SC5110 as SC5a where SC5a.C5_NUM = SC5.C5_NUM and SC5a.D_E_L_E_T_ <> '*') as C5_PBRUTO
  from       CB7110 as CB7
  inner join CB8110 as CB8
@@ -56,17 +57,15 @@ SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C
  and        SC5.D_E_L_E_T_ <> '*'
  inner join SA1110 SA1
  on         A1_COD = C5_CLIENTE
- and
-
-        A1_LOJA = C5_LOJACLI
+ and        A1_LOJA = C5_LOJACLI
  and        SA1.D_E_L_E_T_ <> '*'
  left  join CB1110 CB1
  on         CB1_CODOPE = CB7_XOPERC
  and        CB1.D_E_L_E_T_ <> '*'
  where      CB7.D_E_L_E_T_ <> '*'
  and        CB7_XDTIC = '' and CB7_DTFIMS <> ''
- group by   C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, CB7_PEDIDO, CB7_XHRIC, CB7_XDTIC, CB7_XHRFC, CB7_XDTFC
- ) A GROUP BY C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, CB7_PEDIDO, CB7_XHRIC, CB7_XDTIC, CB7_XHRFC, CB7_XDTFC";
+ group by   C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, CB7_PEDIDO, CB7_DTFIMS, CB7_XHRIC, CB7_XDTIC, CB7_XHRFC, CB7_XDTFC
+ ) A GROUP BY C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, CB7_PEDIDO, CB7_DTFIMS, CB7_XHRIC, CB7_XDTIC, CB7_XHRFC, CB7_XDTFC";
 		}
 
 		if($params['tipo'] == 'conferidos')
@@ -122,9 +121,7 @@ SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C
  and        SC5.D_E_L_E_T_ <> '*'
  inner join SA1110 SA1
  on         A1_COD = C5_CLIENTE
- and
-
-        A1_LOJA = C5_LOJACLI
+ and        A1_LOJA = C5_LOJACLI
  and        SA1.D_E_L_E_T_ <> '*'
  left  join CB1110 CB1
  on         CB1_CODOPE = CB7_XOPERC
@@ -214,6 +211,11 @@ SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C
 							$dt_fim_conf = $this->Common->validaData($row['DT_FIM_CONF']);
 							$hr_fim_conf = $this->Common->validaHora($row['HR_FIM_CONF']);
 				    	}
+						
+						if($row['DT_SEPARACAO'] != '')
+				    	{
+							$dt_separacao = $this->Common->validaData($row['DT_SEPARACAO']);
+				    	}
 
 				    	$_RETURN['row'][] = array(
 				    							'EMISSAO' 		 => $emissao['br_date'],
@@ -229,6 +231,7 @@ SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C
 												'COD_CONFERENTE' => trim($row['COD_CONFERENTE']),
 												'NOM_CONFERENTE' => trim($row['NOM_CONFERENTE']),
 												'PESO_BRUTO' 	 => $row['PESO_BRUTO'],
+												'DT_SEPARACAO'   => $dt_separacao['br_date']
 				    							 );
 
 				    }
@@ -260,3 +263,6 @@ SELECT C5_EMISSAO, C5_NUM, C5_CLIENTE, C5_LOJACLI, A1_NOME, sum(CB8_QTDORI) as C
 	}
 	
 }
+
+
+
